75218309452d29efd089d29d60b0a589fa8af29c
Shir Brunner <shirbrunner@gmail.com>
2019-04-29 23:24:52 +0300

    added touch

--- src/js/directives/timeline.directive.js
+++ src/js/directives/timeline.directive.js
@@ -6,33 +6,67 @@ module.exports = function () {
         link: function (scope, element, attrs) {
             let $timeline = $(element);
             let $progress = $timeline.find('.progress');
+            let $content = $('#content');
             let dragging = false;
+            let lastTouchEvent = null;
+
+            $timeline.on('touchstart', e => {
+                dragging = true;
+                scope.pause();
+                let contentMarginLeft = parseInt($content.css('margin-left'));
+                let offsetX = e.touches.item(0).clientX - $timeline.position().left - contentMarginLeft;
+                let percent = offsetX / $timeline[0].getBoundingClientRect().width * 100;
+                $progress.css('width', percent + '%');
+                scope.onTimelineMove(percent);
+                lastTouchEvent = e;
+            }).on('touchend', e => {
+                if (!dragging)
+                    return;
+
+                let percent = getProgressWidthPercent($progress, $timeline, lastTouchEvent, $content);
+                $progress.css('width', percent + '%');
+
+                scope.onTimelineChange(percent);
+                dragging = false;
+            });
 
             $timeline.on('mousedown', e => {
                 dragging = true;
                 scope.pause();
                 $progress.css('width', e.offsetX + 'px');
                 scope.onTimelineMove(e.offsetX / $timeline.width() * 100);
+                lastTouchEvent = e;
             }).on('mouseup', e => {
-                if(!dragging)
+                if (!dragging)
                     return;
 
                 scope.onTimelineChange(e.offsetX / $timeline.width() * 100);
                 dragging = false;
             });
 
-            $(document).on('mousemove', e => {
+            $(document).on('touchmove', e => {
+                lastTouchEvent = e;
                 if (!dragging)
                     return;
 
-                let percent = getProgressWidthPercent($progress, $timeline, e);
+                let percent = getProgressWidthPercent($progress, $timeline, e, $content);
+                $progress.css('width', percent + '%');
+                scope.onTimelineMove(percent);
+            });
+
+            $(document).on('mousemove', e => {
+                lastTouchEvent = e;
+                if (!dragging)
+                    return;
+
+                let percent = getProgressWidthPercent($progress, $timeline, e, $content);
                 $progress.css('width', percent + '%');
                 scope.onTimelineMove(percent);
             }).on('mouseup', e => {
                 if (!dragging)
                     return;
 
-                let percent = getProgressWidthPercent($progress, $timeline, e);
+                let percent = getProgressWidthPercent($progress, $timeline, e, $content);
                 $progress.css('width', percent + '%');
 
                 scope.onTimelineChange(percent);
@@ -42,8 +76,10 @@ module.exports = function () {
     };
 };
 
-function getProgressWidthPercent($progress, $timeline, e) {
-    let x = e.clientX - $timeline.position().left;
+function getProgressWidthPercent($progress, $timeline, e, $content) {
+    let contentMarginLeft = parseInt($content.css('margin-left'));
+    let clientX = _.get(e, 'touches[0].clientX', e.clientX);
+    let x = clientX - $timeline.position().left - contentMarginLeft;
     let width = $timeline[0].getBoundingClientRect().width;
     let percent = Math.min((x / width) * 100, 100);
     return percent < 0 ? 0 : percent;
